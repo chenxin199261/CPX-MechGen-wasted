21e1f17166e2e971e8fbde50d015ac3c4af5b282
xchen <chenxin199261@gmail.com>
2021-03-29 21:11:59 +0800

    update track file

--- trackTransform.py
+++ trackTransform.py
@@ -3,19 +3,40 @@ import json
 import pickle
 import copy
 
-def trackSpec(tempList,trackList):
+def trackSpec(ftempnames,trackList):
     List = [["a" for i in range(1)] for i in range(len(trackList))]
-    for rec in tempList:
-        i = 0
-        for itm in trackList:
-            # Translate Grp to Name 
-            for rcd in GroupRec_dict:
-                if abs(GroupRec_dict[rcd]-rec[itm][1])<0.00001 :
-                    if (rcd != List[i][-1]):
-                        List[i].append(rcd)
-            i = i+1
+    LastFrame = []
+    # Open different files
+    for ftname in ftempnames:
+        tempList = []
+        print(ftname)
+        with open(ftname, 'rb') as f:
+            tl = pickle.load(f)
+            tempList.extend(tl)
+        for rec in tempList:
+            i = 0
+            for itm in trackList:
+                for rcd in GroupRec_dict:
+                    if abs(GroupRec_dict[rcd]-rec[itm][1])<0.00001 :
+                        if (rcd != List[i][-1]):
+                            List[i].append(rcd)
+                i = i+1
+            LastFrame = rec
+                
+
+
+
+#   for rec in tempList:
+#       i = 0
+#       for itm in trackList:
+#           # Translate Grp to Name 
+#           for rcd in GroupRec_dict:
+#               if abs(GroupRec_dict[rcd]-rec[itm][1])<0.00001 :
+#                   if (rcd != List[i][-1]):
+#                       List[i].append(rcd)
+#           i = i+1
     Ft = open("SpecTrac.txt", 'wb')
-    pickle.dump(tempList, Ft)
+    pickle.dump(List, Ft)
     Ft.close()
 
         
@@ -42,11 +63,10 @@ if __name__ == "__main__":
     trackList = range(2,17191,9)
 
 
-    tempList1=[]
-    for ftname in ftempnames:
-        with open(ftname, 'rb') as f:
-            tl = pickle.load(f)
-            tempList1.extend(tl)
+#   for ftname in ftempnames:
+#       with open(ftname, 'rb') as f:
+#           tl = pickle.load(f)
+#           tempList1.extend(tl)
             
-    trackSpec(tempList1,trackList)
+    trackSpec(ftempnames,trackList)
 
